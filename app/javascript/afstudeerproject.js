70439ed235a0d4bc980859756b37a872c62706a9
David de Lusenet <lusenet@momkai.com>
2016-12-03 13:26:52 +0100

    Added introblock animation

--- app/javascript/afstudeerproject.js
+++ app/javascript/afstudeerproject.js
@@ -1,10 +1,12 @@
 var Slideshow = require('./components/Slideshow');
 var Sidebar = require('./components/Sidebar');
+var IntroBlockAnimation = require('./components/IntroBlockAnimation');
 
 document.addEventListener('DOMContentLoaded', function () {
 
   var slideshows = document.querySelectorAll('.js-slideshow');
   var sidebar = document.querySelector('.js-sidebar');
+  var introBlock = document.querySelector('.js-intro-block');
 
   if (slideshows.length) {
     for (var i = 0; i < slideshows.length; i++) {
@@ -14,6 +16,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   if (sidebar) {
     new Sidebar(sidebar);
+  }
+
+  if (introBlock) {
+    new IntroBlockAnimation(introBlock);
   }
 
 }, false);
---
+++ app/javascript/components/IntroBlockAnimation.js
@@ -0,0 +1,71 @@
+'use strict';
+
+require('gsap');
+
+module.exports = function (element) {
+
+  this.el = element;
+  this.init();
+
+};
+
+module.exports.prototype = {
+
+  init: function () {
+
+    console.log("Init intro block animation");
+
+    this.setVariables();
+    this.initAnimation();
+
+  },
+
+  setVariables: function () {
+
+    this.dividers = this.el.querySelectorAll('.js-intro-block-divider');
+    this.logo = this.el.querySelectorAll('.js-intro-block-logo');
+    this.title = this.el.querySelectorAll('.js-intro-block-title');
+    this.list = this.el.querySelectorAll('.js-intro-block-list');
+
+  },
+
+  initAnimation: function () {
+
+    var timeline = new TimelineMax();
+
+    timeline
+      .set(this.dividers, {
+        width: 0
+      })
+      .set([this.title, this.list], {
+        opacity: 0
+      })
+      .set(this.logo, {
+        position: 'relative',
+        top: '-1000px'
+      })
+      .to(this.el, 0.5, {
+        opacity: 1,
+        ease: Quad.easeInOut
+      })
+      .to(this.logo, 0.8, {
+        top: 0,
+        ease: Quad.easeInOut
+      })
+      .to(this.dividers, 0.4, {
+        width: '100%',
+        ease: Quad.easeInOut
+      })
+      .to(this.title, 0.3, {
+        opacity: 1,
+        ease: Quad.easeInOut
+      }, '-=0.3')
+      .to(this.list, 0.3, {
+        opacity: 1,
+        ease: Quad.easeInOut
+      })
+
+
+  }
+
+};
